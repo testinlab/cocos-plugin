0b3b9486e10c9d4d99fbdaafb54c79b57119d8cb
yincp <yinchunpeng@testin.cn>
2014-10-21 12:17:17 +0800

    changethe encoding of testincc.h to utf8

--- testin/testincc.h
+++ testin/testincc.h
@@ -19,17 +19,17 @@ http://www.testin.cn
 USING_NS_CC;
 
 /**
- *  ����Testin�Խű�¼�Ƶ�֧�֣���Ҫ���ӵ������ʼ���ĵط�
- *  
+ *  启动Testin对脚本录制的支持，需要添加到程序初始化的地方
+ *   
 */
 extern bool StartTestinObserver();
 
 /**
- *  ��ʶ��Ϸ�������������ֲ�ͬ����Ϸ�������棬�Ա����Ϸ���й��������õ�ʶ��
- *  (Ŀǰֻ֧��C++)
+ *  标识游戏场景，用于区分不同的游戏场景界面，以便对游戏运行过程做更好的识别
+ *  (目前只支持C++)
  *  
- *  ������Ϸ��ÿ��CCScene��ʼ���ɹ��󣬵���CCNode::setTag�������Բ�ͬ��CCScene
- *  ���ò�ͬ��tagId���������Բ�����ñ�����
+ *  建议游戏在每个CCScene初始化成功后，调用CCNode::setTag方法，对不同的CCScene
+ *  设置不同的tagId，这样可以不需调用本方法
  *  
 */
 extern void RegisterTestinCCScene(void* scene, const char* name);
